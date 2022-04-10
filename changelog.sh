672c5dff1667089dc6f4ac24cd1ac55532d59e7e
tiulpin <viktor@tiulp.in>
2022-04-10 19:18:07 +0400

    :bricks: Update Actions releaser job

--- changelog.sh
+++ changelog.sh
@@ -4,10 +4,10 @@ for current_tag in $(git tag --sort=-creatordate)
 do
 
 if [ "$previous_tag" != 0 ];then
-    tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
     printf "## Changelog\n"
     git log ${current_tag}...${previous_tag} --pretty=format:'* %h %s' --reverse | grep -v Merge
-    printf "\n\n"
+    printf "\n"
+    break
 fi
 previous_tag=${current_tag}
 done
