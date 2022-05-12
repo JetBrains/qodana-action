b06a3f2289e63df0b6746cfd80f5af9fb6a6c65a
tiulpin <viktor@tiulp.in>
2022-05-12 13:22:11 +0200

    :zap: Update extensions for 2022.1 linters support

--- vsts/QodanaScan/index.js
+++ vsts/QodanaScan/index.js
@@ -101,7 +101,7 @@ function getQodanaScanArgs(args, resultsDir, cacheDir, env = `cli`) {
 var VERSION, EXECUTABLE, FAIL_THRESHOLD_OUTPUT, QODANA_SARIF_NAME, QodanaExitCode;
 var init_qodana = __esm({
   "../common/qodana.ts"() {
-    VERSION = "1.0.4";
+    VERSION = "1.1.0";
     EXECUTABLE = "qodana";
     FAIL_THRESHOLD_OUTPUT = "The number of problems exceeds the failThreshold";
     QODANA_SARIF_NAME = "qodana.sarif.json";
