3fdeefa830a7634a5c04f1cd70fcfb69956137a2
tiulpin <viktor@tiulp.in>
2022-12-08 14:39:02 +0100

    :zap: Update `qodana` to `2022.3.0`

--- scan/__tests__/main.test.ts
+++ scan/__tests__/main.test.ts
@@ -153,15 +153,22 @@ test('check whether Azure Pipelines README.md has the latest major version menti
 
 test('check whether CircleCI orb definition contains the latest version', () => {
   const orb = path.join(__dirname, '..', '..', 'src', 'commands', 'scan.yml')
-  const example = path.join(__dirname, '..', '..', 'src', 'examples', 'scan.yml')
+  const example = path.join(
+    __dirname,
+    '..',
+    '..',
+    'src',
+    'examples',
+    'scan.yml'
+  )
   for (const orbFile of [orb, example]) {
-      const orbFileContent = fs.readFileSync(orbFile, 'utf8')
-      const mentions = orbFileContent.match(/\d+\.\d+\.\d+/g) || []
-      expect(mentions.length > 0).toEqual(true)
-      for (const mention of mentions) {
-        expect(mention).toEqual(VERSION)
-      }
+    const orbFileContent = fs.readFileSync(orbFile, 'utf8')
+    const mentions = orbFileContent.match(/\d+\.\d+\.\d+/g) || []
+    expect(mentions.length > 0).toEqual(true)
+    for (const mention of mentions) {
+      expect(mention).toEqual(VERSION)
     }
+  }
 })
 
 test('download all Qodana CLI archives and check their checksums', async () => {
