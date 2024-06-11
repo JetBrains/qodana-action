0c8f4a68a67192ac922d409a83e6098ac5c33c62
tiulpin <viktor@tiulp.in>
2024-06-12 01:03:15 +0200

    :bricks: Share Kotlin code about CLI to a public JAR

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "qodana"
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+include(
+    ":plugin",
+    ":common",
+)
