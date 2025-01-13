fab6dfd5e7ece406ef7138e8f22d74390bf551e6
Ivan Efimov <Ivan.Efimov@jetbrains.com>
2025-01-13 10:12:45 +0200

    :bug: Fix current branch ref and add cache unit tests (QD-10655)

---
+++ scan/__tests__/utils.test.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021-2025 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {expect} from '@jest/globals'
+import {isNeedToUploadCache} from '../src/utils'
+import * as github from '@actions/github'
+import {ENABLE_USE_CACHE_OPTION_WARNING} from '../src/utils'
+
+const masterBranch = 'master'
+describe('isNeedToUploadCache', () => {
+  it.each([
+    [masterBranch, true],
+    ['branch', false]
+  ])(
+    'for branch %s should return %s when cacheDefaultBranchOnly=true, useCache=true',
+    (currentBranch, expected) => {
+      initGithubContext(currentBranch)
+      expect(isNeedToUploadCache(true, true)).toBe(expected)
+    }
+  )
+
+  it.each([
+    [true, masterBranch, true],
+    [true, 'branch', true],
+    [false, masterBranch, false],
+    [false, 'branch', false]
+  ])(
+    'if cacheDefaultBranchOnly=false should return useCache, current branch is %s, useCache=%s, expected %s',
+    (useCache, currentBranch, expected) => {
+      initGithubContext(currentBranch)
+      expect(isNeedToUploadCache(useCache, false)).toBe(expected)
+    }
+  )
+
+  it.each([masterBranch, 'branch'])(
+    'should return false when useCache=false and cacheDefaultBranchOnly=true, branch %s',
+    branch => {
+      initGithubContext(branch)
+      expect(isNeedToUploadCache(false, true)).toBe(false)
+    }
+  )
+
+  it('should warn when useCache=false and cacheDefaultBranchOnly=true', () => {
+    const core = require('@actions/core')
+    jest.spyOn(core, 'warning')
+    initGithubContext(masterBranch)
+    isNeedToUploadCache(false, true)
+    expect(core.warning).toBeCalledWith(ENABLE_USE_CACHE_OPTION_WARNING)
+  })
+})
+
+export function initGithubContext(currentBranch: string): void {
+  Object.defineProperty(github, 'context', {
+    value: {
+      ref: `refs/heads/${currentBranch}`,
+      payload: {repository: {default_branch: masterBranch}}
+    }
+  })
+}
