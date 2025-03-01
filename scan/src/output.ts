fa2dc349e881a907cf9c2c60905acb561b47bc4a
okue <nogideca@gmail.com>
2025-03-01 19:13:40 +0900

    :bug: Fix annotation path incorrectness when --project-dir is specified

--- scan/src/output.ts
+++ scan/src/output.ts
@@ -90,7 +90,10 @@ export async function publishOutput(
     return
   }
   try {
-    const problems = parseSarif(`${resultsDir}/${QODANA_SARIF_NAME}`)
+    const problems = parseSarif(
+      `${resultsDir}/${QODANA_SARIF_NAME}`,
+      projectDir
+    )
     const reportUrl = getReportURL(resultsDir)
     const coverageInfo = getCoverageStats(
       getCoverageFromSarif(`${resultsDir}/${QODANA_SHORT_SARIF_NAME}`),
