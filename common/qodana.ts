bd2a12e76a5c42af958f9694bfd218c7fbd24888
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2025-08-05 15:43:53 +0200

    Revert ":bug: QD-12336 Update function getQodanaPullArgs, take care of --image param"
    This reverts commit 348009ff859e719f2b81db7f265d427b22867031.

--- common/qodana.ts
+++ common/qodana.ts
@@ -155,12 +155,6 @@ export function getQodanaPullArgs(args: string[]): string[] {
   if (linter) {
     pullArgs.push('-l', linter)
   }
-
-  const image = extractArg('--image', '--image', args)
-  if (linter) {
-    pullArgs.push('--image', image)
-  }
-
   const project = extractArg('-i', '--project-dir', args)
   if (project) {
     pullArgs.push('-i', project)
